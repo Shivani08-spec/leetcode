89b83171a1a82fa293d0c3f6f568604dfbfcb16e
Shivani Rana <62942190+Shivani08-spec@users.noreply.github.com>
2022-05-03 10:15:24 +0530

    Added solution - LeetHub

---
+++ Longest Distinct characters in string - GFG/longest-distinct-characters-in-string.cpp
@@ -0,0 +1,51 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+int longestSubstrDistinctChars (string S);
+int main()
+{
+    int t; cin >> t;
+    while (t--)
+    {
+        string S; cin >> S;
+
+        cout << longestSubstrDistinctChars (S) << endl;
+    }
+}
+
+// Contributed By: Pranay Bansal
+// } Driver Code Ends
+
+
+int longestSubstrDistinctChars (string S)
+{
+    // your code here
+    unordered_map<int,int>mp;
+    int i=0,j=0,mx =0;
+    int n =S.size();
+    while(j<n){
+        mp[S[j]]++;
+        if(mp.size()>j-i+1){
+            j++;
+        }
+        else if(mp.size()==j-i+1){
+            mx = max(mx,j-i+1);
+            j++;
+        }
+        else if(mp.size()<j-i+1){
+            while(mp.size()<j-i+1){
+                mp[S[i]]--;
+                if(mp[S[i]]==0){
+                    mp.erase(S[i]);
+                }
+                    i++;
+                
+                
+            }j++;
+            
+        }
+      
+    }
+      return mx;
+    
+}
