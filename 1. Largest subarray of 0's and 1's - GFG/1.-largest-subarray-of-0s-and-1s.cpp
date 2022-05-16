3751d3baece90e32f492f860de613ad30d253c6c
Shivani Rana <62942190+Shivani08-spec@users.noreply.github.com>
2022-05-16 11:56:43 +0530

    Added solution - LeetHub

---
+++ 1. Largest subarray of 0's and 1's - GFG/1.-largest-subarray-of-0s-and-1s.cpp
@@ -0,0 +1,67 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+/*You are required to complete this method*/
+
+// arr[] : the input array containing 0s and 1s
+// N : size of the input array
+
+// return the maximum length of the subarray
+// with equal 0s and 1s
+class Solution{
+  public:
+    int maxLen(int arr[], int N)
+    {
+        // Your code here
+        map<int,int>mp;
+        int sum =0;
+        int lsub = 0;
+        
+        for(int i=0;i<N;i++){
+            if(arr[i]==1){
+                sum +=1; 
+            }
+            else{
+                sum -= 1;
+            }
+            if(sum ==0){
+                if(lsub<=i+1){
+                    lsub =i+1;
+                    
+                }
+            }
+            
+            else if(mp.find(sum)!=mp.end()){
+                lsub = max(lsub,i-mp[sum]);
+            }
+            else{
+                mp[sum]=i;
+            }
+        }
+        return lsub;
+    }
+};
+
+
+// { Driver Code Starts.
+
+int main()
+{
+    // your code goes here
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        int n;
+        cin >> n;
+        int a[n];
+        for (int i = 0; i < n; i++)
+            cin >> a[i];
+        Solution obj;
+        cout << obj.maxLen(a, n) << endl;
+    }
+    return 0;
+}  // } Driver Code Ends
