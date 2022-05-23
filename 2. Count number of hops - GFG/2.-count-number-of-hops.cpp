64f87b4b78c2df9466c8a9c5ce3059a8b00b32c0
Shivani Rana <62942190+Shivani08-spec@users.noreply.github.com>
2022-05-23 15:34:12 +0530

    Added solution - LeetHub

--- 2. Count number of hops - GFG/2.-count-number-of-hops.cpp
+++ 2. Count number of hops - GFG/2.-count-number-of-hops.cpp
@@ -13,11 +13,12 @@ class Solution
         int mod = 1e9+7;
         // your code here
         long long dp[n+1];
-        dp[0]=1;
+        
         dp[1]=1;
         dp[2]=2;
+        dp[3]=4;
         
-        for(int i=3;i<=n;i++){
+        for(int i=4;i<=n;i++){
             dp[i]=(dp[i-1]%mod+dp[i-2]%(mod)+dp[i-3]%(mod))%(mod);
         }
         return dp[n]%(mod);
